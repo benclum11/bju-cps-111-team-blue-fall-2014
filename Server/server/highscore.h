bb74fc0b7aef5e74105b260ded785d75e6185f86
Ben Clum <benclum11@gmail.com>
2014-10-23 18:33:55 -0400

    Contains files

---
+++ Server/server/highscore.h
@@ -0,0 +1,14 @@
+#ifndef HIGHSCORE_H
+#define HIGHSCORE_H
+
+class Highscore {
+
+    int finalscore;
+
+    Highscore(int finalscore) {
+        //open highscore file and store in finalscore
+    }
+
+};
+
+#endif // HIGHSCORE_H
