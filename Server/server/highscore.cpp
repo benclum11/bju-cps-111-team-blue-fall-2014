e06c62e520b35827a2506ddc424bcaddc8866a8b
Isaac.Best <isaacbest@gmail.com>
2014-10-24 19:53:45 -0400

    Designed some highscore methods. Still needs load/save to/from file methods.

--- Server/server/highscore.cpp
+++ Server/server/highscore.cpp
@@ -1,1 +1,31 @@
+//open highscore file
+//compare if finalScore is higher than highScore
+//set new highscore and name
+//allow viewing of current highscore and highscore-ee
+
 #include "highscore.h"
+
+HighScore::HighScore()
+{
+
+}
+
+//Returns true if given score is higher than current high-score. Stores score in newHScore.
+bool HighScore::checkHScore(int finalScore)
+{
+    if (finalScore > currentHScore)
+    {
+        newScore = finalScore;
+        return true;
+    } else
+    {
+        return false;
+    }
+}
+
+//Sets <score> as new high-score and <highSName> as new highscore-ee
+void HighScore::newHScore(std::string newName)
+{
+    currentHScore = newScore;
+    highSName = newName;
+}
