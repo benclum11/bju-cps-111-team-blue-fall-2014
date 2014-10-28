d4480dfef95dd98136d55b1491926ef8a31c7ffc
Isaac.Best <isaacbest@gmail.com>
2014-10-28 10:52:12 -0400

    Added initial network logic to client. dataReceived & serverDisconnected need to be completed. Port numbers need to be changed to comply with the server specs. (Amended commit.)

--- Client/client/mainwindow.cpp
+++ Client/client/mainwindow.cpp
@@ -1,12 +1,17 @@
 #include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <string>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::dataReceived);
+    connect(socket, &QTcpSocket::disconnected, this, &MainWindow::serverDisconnected);
 }
 
 MainWindow::~MainWindow()
@@ -21,10 +26,34 @@ void MainWindow::on_btnExit_clicked()
 
 void MainWindow::on_btnStart_clicked()
 {
+    //this->netAddress = ui->lnIPAddr->text();
+    if (this->ui->lnIPAddr->text().size() == 0)
+    {
+        socket->connectToHost("localhost", 5000); //Change port number.
+    }
+    else
+    {
+    socket->connectToHost(ui->lnIPAddr->text(), 5000); //The port needs to be cnahged to whatever port we are actually using.
+    }
 
+    if (!socket->waitForConnected())
+    {
+        QMessageBox::critical(this, "Error", "Unable to connect to server.");
+        return;
+    }
 }
 
 void MainWindow::on_btnHelp_clicked()
 {
     QMessageBox::information(this, "Help", "Press Start to start a game. Enter an IP address in the text box to connect to a remote game, otherwise leave blank. Press Exit to quit.");
+}
+
+void MainWindow::dataReceived()
+{
+
+}
+
+void MainWindow::serverDisconnected()
+{
+
 }
