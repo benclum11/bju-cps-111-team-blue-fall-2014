2d3f44fcd8ae6483e1db1bddd4234f983deca06e
Tim <timothy.mk7@gmail.com>
2014-11-10 20:18:06 -0500

    took build errors out

--- Server/server/save.cpp
+++ Server/server/save.cpp
@@ -7,7 +7,7 @@ using namespace std;
 Save::Save(QString filename) {
     filename.replace(QString("\n"), QString(""));
     QFile file;
-    file.setFileName(QString(filename + ".txt");
+    file.setFileName(filename + ".txt");
     file.open(QIODevice::ReadWrite | QIODevice::Text);
     saveFile(file);
     file.close();
