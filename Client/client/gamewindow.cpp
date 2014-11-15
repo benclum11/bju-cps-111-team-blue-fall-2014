ae4b742f29849d5ac358825dd2269addcd21d3bf
Isaac.Best <isaacbest@gmail.com>
2014-11-14 19:28:04 -0500

    Added enemy health display. Changed help menu text.

--- Client/client/gamewindow.cpp
+++ Client/client/gamewindow.cpp
@@ -101,6 +101,12 @@ void GameWindow::createLabelsandButtons()
     health->raise();
     health->show();
 
+    enemyHealth = new QLabel(this->actionDisplay);
+    enemyHealth->setGeometry(50, 300, 200, 50);
+    enemyHealth->setText("Enemy Health: 20");
+    enemyHealth->raise();
+    enemyHealth->show();
+
     //button to start the timer
     startBtn = new QPushButton("Start/Pause Timer", this);
     connect(startBtn, &QPushButton::clicked, this, &GameWindow::on_start_clicked);
@@ -360,6 +366,9 @@ void GameWindow::getPlayerHealth(QString command)
 
     if (BuildableLabel::getClientTeam() == commandArgs.at(1).toInt()) {
         health->setText(QString("Current Health: ") + commandArgs.at(2));
+    } else
+    {
+        enemyHealth->setText(QString("Enemy Health: ") + commandArgs.at(2));
     }
 }
 
@@ -373,7 +382,7 @@ void GameWindow::getPlayerMoney(QString command)
     }
 }
 
-// Depricated
+// Deprecated
 void GameWindow::getPlayerHealthMoney(QString command)
 {
 //    QStringList commandArgs = command.split(" ");
